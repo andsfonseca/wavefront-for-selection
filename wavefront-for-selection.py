a234c7eff9c075018dd89ed6e8549c067c7fb4af
andsfonseca <andsf@live.com>
2021-08-24 22:47:13 -0300

    feat: creating obj and mtl

--- wavefront-for-selection.py
+++ wavefront-for-selection.py
@@ -1,5 +1,60 @@
+from io import StringIO
+import os
 import sys
 
+class WavefrontForSelection:
+    
+    @staticmethod
+    def perform(original_path, result_path = ""):
+
+        #Definitions
+        original_folder_path = os.path.dirname(original_path)
+        original_filename = os.path.basename(original_path)
+        original_basename = os.path.splitext(original_filename)[0]
+
+        if not result_path:
+            result_folder_path = original_folder_path
+            result_basename = original_basename + "_picking"
+            result_filename = result_basename + ".obj"
+            result_mtl_filename = result_basename + ".mtl"
+        else:
+            result_folder_path = os.path.dirname(result_path)
+            result_filename = os.path.basename(result_path)
+            result_basename = os.path.splitext(result_filename)[0]
+            result_mtl_filename = result_basename + ".mtl"
+        
+        result_full_path = result_folder_path + "\\" + result_filename
+        result_mtl_full_path = result_folder_path + "\\" + result_mtl_filename 
+
+        #Create MTLFile
+        mtl_file = open(result_mtl_full_path,"w")
+
+        #Create OBJFile 
+        obj_file = open(result_full_path,"w")
+
+        #Initialize OBJFile
+        obj_file.write(f"mtllib {result_mtl_filename}\n")
+
+        #Reading Wavefront
+        with open(original_path) as f:
+            for line in f:
+
+               split = str.split(line, " ", 1)[0]
+
+               if(split[0] == "o"):
+                   
+                   #Create New Color ID
+
+                   #Associate to MTL
+
+                   #Associate to OBJ
+                   pass        
+
+        mtl_file.close()
+        obj_file.close()
+
+        pass
+    
 if __name__ == "__main__":
 
     def cli():
@@ -26,7 +81,6 @@ Options:
         #Second Argument - Result File Path
         result_file_path = "" if n_args < 3 else sys.argv[2]
 
-        print(original_file_path)
-        print(result_file_path)
+        WavefrontForSelection.perform(original_file_path, result_file_path)
 
     cli()
